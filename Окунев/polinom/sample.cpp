bfcd5c40483e01584682238770235ac3a30bb8bf
Boris Okunev <hait.08@mail.ru>
2017-04-09 00:27:15 +0300

    Изменена графическая составляющая примера

--- Окунев/polinom/sample.cpp
+++ Окунев/polinom/sample.cpp
@@ -19,10 +19,14 @@ int main()
 	cout << "Введите полином p2\n";
 	cin >> std;
 	TPolinom polinom2(std);
+    cout << "--------------------------" << endl;
+    cout << "Введённые полиномы :" << endl;
 	cout << polinom.ToString() << endl;
 	cout << polinom2.ToString() << endl;
     //пример арифметических операций
 	TPolinom polinom3;
+    cout << "--------------------------" << endl;
+    cout << "Арифметические операции :" << endl;
     cout << "p1 + p2 " << endl;
 	polinom3 = polinom + polinom2;
 	cout << polinom3.ToString() << endl;
@@ -30,8 +34,9 @@ int main()
     polinom3 = polinom - polinom2;
     cout << polinom3.ToString() << endl;
     //пример подсчета полинома с указанными значениями
-    double val[3];
+    cout << "--------------------------" << endl;
     cout << "Введите значения x, y, z" << endl;
+    double val[3];
     for (int i = 0; i < 3; i++)
     {
         cin >> val[i];
